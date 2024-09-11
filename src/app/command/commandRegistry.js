1d67265fafeb2318d71be1e56e050718f7ff84dd
mmeerrkkaa <merkkaa81@gmail.com>
2024-09-11 23:53:26 +0300

    Update commandRegistry.js

--- src/app/command/commandRegistry.js
+++ src/app/command/commandRegistry.js
@@ -20,82 +20,108 @@ const logger = winston.createLogger({
 const fileToCommandNameMap = {};
 const commands = {};
 
-async function loadCommands() {
-    const commandsDir = path.join(process.cwd(), 'commands');
-    console.log(`Loading commands from directory: ${commandsDir}`);
-
-    const loadCommand = async (filePath) => {
-        try {
-            const stat = await fs.promises.stat(filePath);
-            if (!stat.isFile()) {
-                return;
-            }
-
-            delete require.cache[require.resolve(filePath)];
-            const CommandClass = require(filePath);
-            if (typeof CommandClass === 'function') {
-                const commandInstance = new CommandClass();
-                if (commandInstance.name) {
-                    const commandNames = Array.isArray(commandInstance.name) ? commandInstance.name : [commandInstance.name];
-                    const commandVariations = commandInstance.variations || [];
-                    const allNames = commandNames.concat(commandVariations);
-
-                    allNames.forEach(commandName => {
-                        commands[commandName] = commandInstance;
-                        logger.info(`Команда ${commandName} загружена.`);
-                    });
-                    fileToCommandNameMap[filePath] = allNames;
-                } else {
-                    logger.error(`Ошибка: Не найдено имя для команды в файле ${filePath}`);
-                }
-            } else {
-                logger.error(`Ошибка: ${filePath} не является конструктором класса.`);
-            }
-        } catch (error) {
-            logger.error(`Ошибка при загрузке команды из файла ${filePath}: ${error.message}`);
-        }
-    };
-
+const loadCommand = async (filePath) => {
     try {
-        const dirExists = await fs.promises.access(commandsDir).then(() => true).catch(() => false);
-
-        if (!dirExists) {
-            logger.warn(`Директория команд не существует: ${commandsDir}`);
-            return commands;
+        const stat = await fs.promises.stat(filePath);
+        if (!stat.isFile()) {
+            return;
         }
 
-        const files = await fs.promises.readdir(commandsDir);
+        delete require.cache[require.resolve(filePath)];
+        const CommandClass = require(filePath);
+        if (typeof CommandClass === 'function') {
+            const commandInstance = new CommandClass();
+            if (commandInstance.name) {
+                const commandNames = Array.isArray(commandInstance.name) ? commandInstance.name : [commandInstance.name];
+                const commandVariations = commandInstance.variations || [];
+                const allNames = commandNames.concat(commandVariations);
 
-        if (files.length === 0) {
-            logger.warn(`Команды не обнаружены в директории: ${commandsDir}`);
+                allNames.forEach(commandName => {
+                    commands[commandName] = commandInstance;
+                    logger.info(`Команда ${commandName} загружена.`);
+                });
+                fileToCommandNameMap[filePath] = allNames;
+            } else {
+                logger.error(`Ошибка: Не найдено имя для команды в файле ${filePath}`);
+            }
         } else {
-            for (const file of files) {
-                const filePath = path.join(commandsDir, file);
-                console.log(`Loading command from file: ${filePath}`);
+            logger.error(`Ошибка: ${filePath} не является конструктором класса.`);
+        }
+    } catch (error) {
+        logger.error(`Ошибка при загрузке команды из файла ${filePath}: ${error.message}`);
+    }
+};
+
+const loadCommandsFromDirectory = async (commandsDir) => {
+    try {
+        const files = await fs.promises.readdir(commandsDir, { withFileTypes: true });
+
+        for (const file of files) {
+            const filePath = path.join(commandsDir, file.name);
+            if (file.isDirectory()) {
+                // Рекурсивно загружаем команды из поддиректорий
+                await loadCommandsFromDirectory(filePath);
+            } else {
                 await loadCommand(filePath);
             }
         }
     } catch (error) {
-        logger.warn(`Ошибка при начальной загрузке команд: ${error.message}`);
+        logger.warn(`Ошибка при загрузке команд из директории ${commandsDir}: ${error.message}`);
+    }
+};
+
+const loadPluginCommandDirectories = async () => {
+    const rootDir = path.join(process.cwd(), 'plugins');
+    const pluginDirs = [];
+
+    try {
+        const subDirs = await fs.promises.readdir(rootDir, { withFileTypes: true });
+        for (const subDir of subDirs) {
+            if (subDir.isDirectory()) {
+                const commandDirPath = path.join(rootDir, subDir.name, 'src/command');
+                const exists = await fs.promises.access(commandDirPath).then(() => true).catch(() => false);
+                if (exists) {
+                    pluginDirs.push(commandDirPath);
+                }
+            }
+        }
+    } catch (error) {
+        logger.error(`Ошибка при поиске директорий плагинов: ${error.message}`);
     }
 
-    const watcher = chokidar.watch(commandsDir, { ignored: /^\./, persistent: true });
-    watcher
-        .on('add', loadCommand)
-        .on('change', async (filePath) => {
-            await loadCommand(filePath);
-        })
-        .on('unlink', async (filePath) => {
-            const commandNames = fileToCommandNameMap[filePath];
-            if (commandNames) {
-                commandNames.forEach(commandName => {
-                    delete commands[commandName];
-                });
-                delete fileToCommandNameMap[filePath];
-                logger.info(`Команда ${commandNames.join(', ')} удалена из-за удаления файла.`);
-            }
-        })
-        .on('error', error => logger.error(`Ошибка наблюдателя: ${error.message}`));
+    return pluginDirs;
+};
+
+async function loadCommands() {
+    const commandsDir = path.join(process.cwd(), 'commands');
+    console.log(`Loading commands from directory: ${commandsDir}`);
+
+    await loadCommandsFromDirectory(commandsDir);
+
+    const pluginCommandDirs = await loadPluginCommandDirectories();
+
+    for (const pluginCommandDir of pluginCommandDirs) {
+        console.log(`Loading plugin commands from directory: ${pluginCommandDir}`);
+        await loadCommandsFromDirectory(pluginCommandDir);
+
+        const watcher = chokidar.watch(pluginCommandDir, { ignored: /^\./, persistent: true });
+        watcher
+            .on('add', loadCommand)
+            .on('change', async (filePath) => {
+                await loadCommand(filePath);
+            })
+            .on('unlink', async (filePath) => {
+                const commandNames = fileToCommandNameMap[filePath];
+                if (commandNames) {
+                    commandNames.forEach(commandName => {
+                        delete commands[commandName];
+                    });
+                    delete fileToCommandNameMap[filePath];
+                    logger.info(`Команда ${commandNames.join(', ')} удалена из-за удаления файла.`);
+                }
+            })
+            .on('error', error => logger.error(`Ошибка наблюдателя: ${error.message}`));
+    }
 
     if (Object.keys(commands).length === 0) {
         logger.warn('Нет доступных команд после загрузки.');
